06ccb1538ec9ac79e01e41e2f77b918fe63fd492
indrayanikalake <indraayanikalaake333@gmail.com>
2023-07-24 15:59:41 +0530

    subscribing and dispatching updated

--- src/Redux/Counter.jsx
+++ src/Redux/Counter.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { slideIn } from '../utils/motion';
 import { styles } from '../styles';
-import { useSelector, connect } from 'react-redux';
+import { useSelector, connect, useDispatch } from 'react-redux';
 
 
 const Counter = () => {
+    const dispatch = useDispatch();
     const counter = useSelector(state=> state.count);
+
+    const increamentHandler = () =>{
+        dispatch({type:'INCREMENTBY5'});
+    }
+
+    const decreamentHandler = () =>{
+        dispatch({type: 'DECREMENTBY5'});
+    }
+
     const toggleCounterHandler = () =>{};
 
   return (
@@ -16,6 +26,10 @@ const Counter = () => {
     <form className='mt-2 flex flex-col gap-2'>
       <h1 className={`${styles.heroHeadText} text-center`}>Redux Counter</h1>
       <h3 className='text-center text-white text-medium'>{counter}</h3>
+      <div className='flex flex-row items-center justify-center space-x-4px'>
+        <button type='button' onClick={increamentHandler}>Increament</button>
+        <button type='button' onClick={decreamentHandler}>Decreament</button>
+      </div>
       <button type='button' onClick={toggleCounterHandler}>Toggle Counter</button>
     </form>
     </motion.div>
--- src/Redux/Redux.jsx
+++ src/Redux/Redux.jsx
@@ -6,10 +6,10 @@ import { createStore } from 'redux';
   // Reducer function
   const counterReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
-      case 'INCREMENT':
-        return { ...state, count: state.count + 1 };
-      case 'DECREMENT':
-        return { ...state, count: state.count - 1 };
+      case 'INCREMENTBY5':
+        return { ...state, count: state.count + 5 };
+      case 'DECREMENTBY5':
+        return { ...state, count: state.count - 5 };
       case 'INCREMENTBY2':
         return { ...state, count: state.count + 2 };
       case 'DECREMENTBY2':
@@ -29,11 +29,11 @@ import { createStore } from 'redux';
   });
   
   // Dispatch actions to update the state
-  store.dispatch({ type: 'INCREMENT' }); // Output: { count: 1 }
+  store.dispatch({ type: 'INCREMENTBY5' }); // Output: { count: 1 }
   store.dispatch({ type: 'INCREMENTBY2' }); // Output: { count: 3 }
   store.dispatch({ type: 'INCREMENTBY2' }); // Output: { count: 5 }
   
-  store.dispatch({ type: 'DECREMENT' }); // Output: { count: 4 }
+  store.dispatch({ type: 'DECREMENTBY5' }); // Output: { count: 4 }
   store.dispatch({ type: 'DECREMENTBY2' }); // Output: { count: 2 }
   
 export default store;
